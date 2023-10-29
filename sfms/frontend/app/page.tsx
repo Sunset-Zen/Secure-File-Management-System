8b0fc554f86e1499b06d2667f6f05a6f7b13e169
Otis Jackson IV <142633964+Sunset-Zen@users.noreply.github.com>
2023-10-29 09:59:29 -0500

    User input update
    Console.log()'s user credentials in a JSON format of an object named data.

--- sfms/frontend/app/page.tsx
+++ sfms/frontend/app/page.tsx
@@ -1,23 +1,20 @@
+"use client";
 import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
 
 function page() {
   // Attributes | Hook(s)
+  const [page, setPage] = useState();
 
   // TSX
   return (
     <div>
+      {/* Title */}
       <div>
-        <h1>Secure File Management System : Team B</h1>
+        <h1 className="text-xl">Secure File Management System : Team B</h1>
       </div>
-
-      <form id="input_form">
-        <label title="username">Username : </label>
-        <input type="text" className="input" />
-        <label title="password">Password : </label>
-        <input type="password" className="input" />
-
-        <button id="submit_button">Submit</button>
-      </form>
+      {/* User Input Form */}
+      <Form />
     </div>
   );
 }
