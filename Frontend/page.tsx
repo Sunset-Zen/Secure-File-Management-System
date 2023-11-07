486e7ce0a084ffcbce15300535bad54fd6c297e8
Otis Jackson IV <142633964+Sunset-Zen@users.noreply.github.com>
2023-11-06 23:34:26 -0600

    Initial Set Up (Revamp) -> MVC
    proper initial frontend + backend set up

---
+++ Frontend/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+// import { FormProvider } from "./components/FormContext";
+// import { FormStep } from "./components/FormStep";
+
+function page() {
+  // Attributes | Hook(s)
+
+  // TSX
+  return (
+    <div>
+      {/* Title */}
+      <div>
+        <h1 className="text-xl font-semibold">
+          Secure File Management System : Team B
+        </h1>
+      </div>
+
+      {/* User Input Form */}
+      {/* <FormProvider>
+        <FormStep />
+      </FormProvider> */}
+    </div>
+  );
+}
+
+export default page;
